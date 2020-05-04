5507ae4f2945f699f83e9a5acfbbb58d4f3cd631
serena-ramley <sramley@gmail.com>
2020-05-03 18:20:22 -0700

    Set up sign in form

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 
 import ShopPage from "./pages/shop/shop.component";
 import HomePage from "./pages/homepage/homepage.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component.jsx";
 
 // const Detail = (props) => (
@@ -33,9 +34,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
-        {/* <Route exact path="/itemlist" component={List} />
-        <Route exact={false} path="/itemlist/:topicid" component={Detail} /> */}
-        {/* <HomePage /> */}
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
