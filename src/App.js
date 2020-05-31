89e62f44b9cf4302037b8942fdcffae3746b284e
serena-ramley <sramley@gmail.com>
2020-05-31 11:21:27 -0700

    Styled Google sign-in button

--- src/App.js
+++ src/App.js
@@ -7,37 +7,39 @@ import ShopPage from "./pages/shop/shop.component";
 import HomePage from "./pages/homepage/homepage.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component.jsx";
+import { auth } from "./firebase/firebase.utils";
 
-// const Detail = (props) => (
-//   <div>
-//     console.log(props)
-//     <h1>DETAIL PAGE</h1>
-//   </div>
-// );
+class App extends React.Component {
+  constructor() {
+    super();
 
-// const List = () => (
-//   <div>
-//     <h1>LIST PAGE</h1>
-//   </div>
-// );
+    this.state = {
+      currentUser: null,
+    };
+  }
 
-const FlowersPage = () => (
-  <div>
-    <h1>FLOWERS PAGE</h1>
-  </div>
-);
+  unsubscribeFromAuth = null;
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/signin" component={SignInAndSignUpPage} />
-      </Switch>
-    </div>
-  );
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      auth.onAuthStateChanged((user) => {
+        this.setState({ currentUser: user });
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
