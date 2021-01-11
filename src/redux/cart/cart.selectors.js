9274152b3219addaa2182bfef05706a4adb60444
serena-ramley <sramley@gmail.com>
2021-01-10 22:55:53 -0800

    Fixed typo

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -1,6 +1,6 @@
 import { createSelector } from "reselect";
 
-const selectCart = (state) => state.selectCart;
+const selectCart = (state) => state.cart;
 
 export const selectCartItems = createSelector(
   [selectCart],
