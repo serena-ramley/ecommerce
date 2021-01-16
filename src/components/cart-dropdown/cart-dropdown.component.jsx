981c0627e65dc0fcc44d20e389f346a586d15456
serena-ramley <sramley@gmail.com>
2021-01-16 09:50:03 -0800

    Fixed cart dropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -12,14 +12,15 @@ import "./cart-dropdown.styles.scss";
 
 const CartDropdown = ({ cartItems, history, dispatch }) => (
   <div className="cart-dropdown">
-    <div className="cart-items" />
-    {cartItems.length ? (
-      cartItems.map((cartItem) => (
-        <CartItem key={cartItem.id} item={CartItem} />
-      ))
-    ) : (
-      <span className="empty-message">Your cart is empty</span>
-    )}
+    <div className="cart-items">
+      {cartItems.length ? (
+        cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))
+      ) : (
+        <span className="empty-message">Your cart is empty</span>
+      )}
+    </div>
     <CustomButton
       onClick={() => {
         history.push("/checkout");
