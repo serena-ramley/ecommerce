9d948c69d29b478c88fde80676fd143d9cd57fed
serena-ramley <sramley@gmail.com>
2021-01-18 11:02:39 -0800

    Added custom button styles with styled-components

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 
-import "./custom-button.styles.scss";
+import { CustomButtonContainer } from "./custom-button.styles";
+// import "./custom-button.styles.scss";
 
-const CustomButton = ({ children, isGoogleSignIn, inverted, ...props }) => (
-  <button
-    className={`${inverted ? "inverted" : ""} ${
-      isGoogleSignIn ? "google-sign-in" : ""
-    } custom-button`}
-    {...props}
-  >
-    {children}
-  </button>
+const CustomButton = ({ children, ...props }) => (
+  <CustomButtonContainer {...props}>{children}</CustomButtonContainer>
 );
+
+// const CustomButton = ({ children, isGoogleSignIn, inverted, ...props }) => (
+//   <button
+//     className={`${inverted ? "inverted" : ""} ${
+//       isGoogleSignIn ? "google-sign-in" : ""
+//     } custom-button`}
+//     {...props}
+//   >
+//     {children}
+//   </button>
+// );
 
 export default CustomButton;
--- src/pages/homepage/homepage.styles.jsx
+++ src/pages/homepage/homepage.styles.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-const HomePageContainer = styled.div`
+export const HomePageContainer = styled.div`
   .homepage {
     display: flex;
     flex-direction: column;
