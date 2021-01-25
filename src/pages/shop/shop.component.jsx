b19a16c32e7c0eb924a5eea615a570762691b005
serena-ramley <sramley@gmail.com>
2021-01-25 09:30:08 -0800

    Converted from a functional component to a class-based component and removed unused logos

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -3,19 +3,25 @@ import { Route } from "react-router-dom";
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 import CollectionPage from "../collection/collection.component";
 
-const ShopPage = ({ match }) => (
-  <div className="shop-page">
-    <Route
-      exact
-      path={`${match.path}`}
-      render={(props) => <CollectionsOverview {...props} />}
-    />
-    <Route
-      exact
-      path={`${match.path}/:collectionId`}
-      render={(props) => <CollectionPage {...props} />}
-    />
-  </div>
-);
+class ShopPage extends React.component {
+  render() {
+    const { match } = this.props;
+
+    return (
+      <div className="shop-page">
+        <Route
+          exact
+          path={`${match.path}`}
+          render={(props) => <CollectionsOverview {...props} />}
+        />
+        <Route
+          exact
+          path={`${match.path}/:collectionId`}
+          render={(props) => <CollectionPage {...props} />}
+        />
+      </div>
+    );
+  }
+}
 
 export default ShopPage;
